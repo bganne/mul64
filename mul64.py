a0789d87dc5efa0e5690ee4bd5a011201343811d
Benoît Ganne <benoit.ganne@gmail.com>
2016-10-30 20:22:48 -0700

    add numpy array check

--- mul64.py
+++ mul64.py
@@ -39,18 +39,30 @@ if __name__ == '__main__':
         
         def setup(self): pass
 
+        def test_wrong(self):
+            a, b = np.uint64(1), np.uint64(self.MAX_UINT64)
+            self.assertFalse(mul64x64_128(a, b) == 0)
+
         def __check(self, a, b):
             a64, b64 = np.uint64(a), np.uint64(b)
             rhi, rlo = mul64x64_128(a64, b64)
             self.assertEqual(int(a)*int(b), (int(rhi) << 64) + int(rlo))
 
-        def test_short(self):
+        def test_int_limits(self):
             self.__check(self.MAX_UINT64, self.MAX_UINT64)
             self.__check(0, 1)
             self.__check(1, 1)
 
-        def test_long(self):
-            for _ in xrange(1000000):
+        def test_int_rand(self):
+            for _ in xrange(10000):
                 self.__check(random.randint(0, self.MAX_UINT64), random.randint(0, self.MAX_UINT64))
+
+        def test_array_rand(self):
+            a = np.random.randint(0, self.MAX_UINT64, 1000000, dtype=np.uint64)
+            b = np.random.randint(0, self.MAX_UINT64, len(a), dtype=np.uint64)
+            hi, lo = mul64x64_128(a, b)
+            ai, bi = a.astype('object'), b.astype('object')
+            hi, lo = hi.astype('object'), lo.astype('object')
+            self.assertTrue(np.array_equal(ai * bi, ((hi << 64) | lo)))
 
     unittest.main()
